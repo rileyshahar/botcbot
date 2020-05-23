13f70d789bc5d2ec07c2df24eea261052be37209
Riley Shahar <riley.shahar@gmail.com>
2020-05-22 18:31:23 -0700

    Add Empath functionality

--- resources/basegame/characters/Empath.py
+++ resources/basegame/characters/Empath.py
@@ -1,6 +1,8 @@
 """Contains the Empath class."""
 
 from lib.logic.Character import Townsfolk
+from lib.logic.charcreation import if_functioning
+from lib.typings.context import Context
 
 
 class Empath(Townsfolk):
@@ -8,3 +10,26 @@ class Empath(Townsfolk):
 
     name: str = "Empath"
     playtest: bool = False
+
+    @if_functioning(True)
+    async def morning_call(self, ctx: Context, enabled=True, epithet_string="") -> str:
+        """Determine the morning call."""
+        neighbors = self.parent.neighbors(
+            ctx.bot.game, condition=lambda x, y: not x.ghost(y, registers=True)
+        )
+        numb = len(
+            [
+                player
+                for player in neighbors
+                if player and player.is_status(ctx.bot.game, "evil", registers=True)
+            ]
+        )
+        out = (
+            f"Tell {self.parent.formatted_epithet(epithet_string)}, "
+            "a number of evil living neighbors "
+        )
+        if enabled:
+            out += f"(**{numb}**)."
+        else:
+            out += "(this may be any number)."
+        return out
