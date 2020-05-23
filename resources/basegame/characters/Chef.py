2a726fcd3f4e922267d5356d33598f82d1fefc76
Riley Shahar <riley.shahar@gmail.com>
2020-05-22 19:05:02 -0700

    Add Chef functionality

--- resources/basegame/characters/Chef.py
+++ resources/basegame/characters/Chef.py
@@ -1,6 +1,8 @@
 """Contains the Chef class."""
 
 from lib.logic.Character import Townsfolk
+from lib.logic.charcreation import if_functioning
+from lib.typings.context import Context
 
 
 class Chef(Townsfolk):
@@ -8,3 +10,23 @@ class Chef(Townsfolk):
 
     name: str = "Chef"
     playtest: bool = False
+
+    @if_functioning(True)
+    async def morning_call(self, ctx: Context, enabled=True, epithet_string="") -> str:
+        """Determine the morning call."""
+        numb = 0
+        for character in ctx.bot.game.seating_order:
+            if character.is_status(ctx.bot.game, "evil", registers=True):
+                if character.neighbors(ctx.bot.game)[1].is_status(
+                    ctx.bot.game, "evil", registers=True
+                ):
+                    numb += 1
+        out = (
+            f"Tell {self.parent.formatted_epithet(epithet_string)}, "
+            "a number of evil pairs "
+        )
+        if enabled:
+            out += f"(**{numb}**)."
+        else:
+            out += "(this may be any number)."
+        return out
